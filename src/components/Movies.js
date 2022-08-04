f07d0aaa643e4e29f7ebdb49e23ea19efc396e2b
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-08-04 10:19:24 +0300

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+     <h1>Movies Page</h1>
+      {movies.map((movie) => {
+       return <div key={movie.title}>
+          <h3>Name: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres: </p>
+          <ul>
+            {movie.genres.map((genre,i) => (
+              <li key={movie.title+i}> {genre}</li>
+            ))}
+          </ul>
+        </div>;
+      })}
+  </div>
+  );
 }
 
 export default Movies;
